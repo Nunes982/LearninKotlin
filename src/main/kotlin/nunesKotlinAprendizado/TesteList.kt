89300aee93e9565494e4a2561f6be23571f99220
Nunes982 <ansso06061982@gmail.com>
2022-06-16 20:23:08 -0300

    updated

---
+++ src/main/kotlin/nunesKotlinAprendizado/TesteList.kt
@@ -0,0 +1,25 @@
+package nunesKotlinAprendizado
+
+fun main() {
+    val joao = Funcionario("João",2000.0,"CLT")
+    val pedro = Funcionario("Pedro",1500.0, "PJ")
+    val maria = Funcionario("Maria",4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{ println(it) }
+
+    println("--------------------------------------")
+    println(funcionarios.filter { it == maria })
+
+    println("--------------------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{ println(it) }
+
+    println("--------------------------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach{ println(it) }
+}
+
